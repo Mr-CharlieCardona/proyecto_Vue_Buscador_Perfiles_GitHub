405bf6f56a9e66b2511ab889e0da7b6373de5202
cacardona53 <cacardona53@misena.edu.co>
2022-06-02 14:30:08 -0500

    avance 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 const API = "https://api.github.com/users/";
+
+
+
+const app = Vue.createApp({
+    data() {
+        return {
+            message: "Search GitHub users",
+        };
+    },
+    method: {
+        async doSearch() {
+            const response = await fetch(API + 'juanwmedia')
+            const data = await response.json()
+            console.log(data)
+        }
+    }
+});
